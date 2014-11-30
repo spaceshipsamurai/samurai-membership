96876918178224316ded1e04224f1eacf8c1c0bc
Russell Durham <rdurham1007@gmail.com>
2014-11-30 15:14:48 -0600

    Finished up retrieving groups by userid

--- src/group-management/group-membership-service.js
+++ src/group-management/group-membership-service.js
@@ -2,13 +2,13 @@ var Member = require('./member-model'),
     Promise = require('bluebird'),
     Keys = require('../keys/key-service');
 
-exports.removeAllFromGroup = function(groupId) {
+exports.removeAllFromGroup = function (groupId) {
 
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
 
-        Member.find({ group: groupId }).remove(function(err){
+        Member.find({group: groupId}).remove(function (err) {
 
-            if(err) reject(err);
+            if (err) reject(err);
 
             resolve();
 
@@ -17,9 +17,9 @@ exports.removeAllFromGroup = function(groupId) {
     });
 };
 
-exports.apply = function(groupId, application){
+exports.apply = function (groupId, application) {
 
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
 
         Member.create({
             groupId: groupId,
@@ -30,9 +30,9 @@ exports.apply = function(groupId, application){
                 appliedDate: new Date(),
                 status: 'Pending'
             }]
-        }, function(err, member){
+        }, function (err, member) {
 
-            if(err) reject(err);
+            if (err) reject(err);
 
             resolve(member);
 
@@ -42,35 +42,56 @@ exports.apply = function(groupId, application){
 
 };
 
-exports.getByUserId = function(userId) {
+exports.getByUserId = function (userId) {
 
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
 
-        Keys.getCharacters({ userId: userId, validOnly: true }).then(function(characters){
+        Member.find({'userId': userId}).populate('group').exec(function (err, members) {
 
-            var cids = [];
+            if (err) reject(err);
 
-            for(var x = 0; x < characters.length; x++)
-                cids.push(characters[x].id);
+            var groups = {};
 
-            Member.find({ 'characters.id': { $in: cids }}).populate('group').exec(function(err, members){
+            for (var x = 0; x < members.length; x++) {
+                var group = members[x].group.toObject();
+                var managers = [];
 
-                if(err) reject(err);
-
-                var groups = {};
-
-                console.log('MEMBERS: ' + members);
-
-                for(var x = 0; x < members.length; x++)
-                {
-                    groups[members[x].group.name] = members[x].group;
+                if (group.managers) {
+                    managers = group.managers.reduce(function (prev, curr) {
+                        return prev.concat(curr.id);
+                    }, []);
                 }
 
-                resolve(groups);
 
-            });
+                groups[members[x].group.name] = group;
+                groups[members[x].group.name].characters = members[x].characters;
+
+                var overallStatus = 'Inactive';
+
+                for (var c = 0; c < members[x].characters.length; c++) {
+                    if (members[x].characters[c].status === 'Pending' && overallStatus != 'Accepted') {
+                        overallStatus = 'Pending';
+                    }
+                    else if (members[x].characters[c].status === 'Accepted') {
+                        if (group.owner && group.owner.id === members[x].characters[c].id) {
+                            overallStatus = 'Owner';
+                            break;
+                        }
+                        else if(managers.length > 0 && managers.indexOf(members[x].characters[c].id) > -1)
+                            overallStatus = 'Manager';
+                        else if (overallStatus != 'Accepted')
+                            overallStatus = 'Accepted';
+                    }
+                }
+
+                groups[members[x].group.name].memberStatus = overallStatus;
+
+            }
+
+            resolve(groups);
 
         });
+
 
     });
 
